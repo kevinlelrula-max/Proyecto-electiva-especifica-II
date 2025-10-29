4a74e4ee8b182db40c57b1b9cf2e0e211389d316
unknown <kevinlelrula@gmail.com>
2025-10-28 22:37:42 -0500

    añadido carpeta auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import { ProductosProvider } from "./inventario/context";
 import { VentasProvider } from "./puntodeventa/context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <AuthProvider>
       <ProductosProvider>
         <VentasProvider> {/* ✅ ahora sí se incluye */}
