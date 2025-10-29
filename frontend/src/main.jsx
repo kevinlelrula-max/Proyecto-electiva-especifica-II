90238f3b718bf7f358ceda6b91a744b08f3b571b
unknown <kevinlelrula@gmail.com>
2025-10-28 22:36:15 -0500

    actualizacion backend (inicio de sesion)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           
           <App />
         </VentasProvider>
-      </ProductosProvider>
+      </ProductosProvider> 
     </AuthProvider>
   </React.StrictMode>
 );
